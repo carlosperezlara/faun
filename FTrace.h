422d2d29b699fd26a1b00ea973cefab939440b7f
therohboat <william.roh@stonybrook.edu>
2015-08-07 16:31:32 -0400

    test

--- FTrace.h
+++ FTrace.h
@@ -1,2 +1,20 @@
-#ifndef __CTRACE_H__
-#define __CTRACE_H__
+#ifndef __FTRACE_H__
+#define __FTRACE_H__
+#include <vector>
+
+class CTrace {
+  public;
+  FTrace();
+  ~FTrace();
+  double Signal() {return fSignal;}
+  double Time() {return fTime;}
+
+  protected;
+  std::vector<double> voltage;
+  void ComputeSignal();
+  void ComputeTime();
+  double fSignal;
+  double fTime;
+};
+
+#endif /*__FTRACE_H__*/
